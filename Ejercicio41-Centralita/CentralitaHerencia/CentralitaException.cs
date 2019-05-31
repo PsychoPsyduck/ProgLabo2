e5a143a43daebd0682d1a263f9832819bb568bab
psychopsyduck <nialsande@gmail.com>
2019-05-30 22:11:57 -0300

    ej 40

---
+++ Ejercicio41-Centralita/CentralitaHerencia/CentralitaException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class CentralitaException : Exception
+    {
+        private string nombreClase;
+        private string nombreMetodo;
+
+        public CentralitaException(string mensaje, string clase, string metodo) : base(mensaje)
+        {
+            nombreClase = clase;
+            nombreMetodo = metodo;
+        }
+
+        public CentralitaException(string mensaje, string clase, string metodo, Exception innerException) : base(mensaje, innerException)
+        {
+
+        }
+
+        public string NombreClase
+        {
+            get
+            {
+                return nombreClase;
+            }
+        }
+
+        public string NombreMetodo
+        {
+            get
+            {
+                return nombreMetodo;
+            }
+        }
+    }
+}
